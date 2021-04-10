2de16eaf8b77b346bb227dfbe1ba3296b985e351
htf <13619254401@163.com>
2021-04-10 11:41:15 +0800

    2 commit

---
+++ src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,13 @@
+package com.example.demo.test;
+
+/**
+ * @Description: java类作用描述
+ * @Author: huangtf
+ * @CreateDate: 2021/4/10 11:40
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
